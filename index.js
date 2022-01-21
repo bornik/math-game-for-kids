83581b6d3e72caafd6cfaa9af6fe0d116f31175e
bornik <nik.born@gmail.com>
2022-01-21 08:38:48 +0200

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-import React, { useState } from 'react';
-import { render } from 'react-dom';
-import {Game} from './Game';
+import React from 'react';
+import {Landing} from './Landing';
 import './style.css';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-9N289HG8YQ');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
-  const [operation, setOperation] = useState('÷')
-
-  const handleOperationChange = (event) => {
-    setOperation(event.target.value)
-  }
-  
-    return (
-      <div>
-        <h1>Привіт, любий друже!</h1>
-        <h2>Хочеш погратися в математику?</h2>
-        <label htmlFor='operator'>Обери операції, які тобі довподоби:</label>&nbsp;
-        <select value={operation} onChange={handleOperationChange} id='operator'>
-          <option value='÷'>÷</option>
-          <option value='*'>*</option>
-          <option value='+'>+</option>
-          <option value='-'>-</option>
-        </select>
-        <Game type={operation}/>
-      </div>
-    );
-
+  return <Landing/>
 }
-
 render(<App />, document.getElementById('root'));
