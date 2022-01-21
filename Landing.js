47887266c12958dd3b274a238b9ad7aa89f9fdba
bornik <nik.born@gmail.com>
2022-01-21 08:37:24 +0200

    2 files modified, 1 added

---
+++ Landing.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import {Game} from './Game';
+
+export const Landing = () => {
+  const [operation, setOperation] = useState('÷')
+
+  const handleOperationChange = (event) => {
+    setOperation(event.target.value)
+  }
+  
+    return (
+      <div>
+        <h1>Привіт, любий друже!</h1>
+        <h2>Хочеш погратися в математику?</h2>
+        <label htmlFor='operator'>Обери операції, які тобі довподоби:</label>&nbsp;
+        <select value={operation} onChange={handleOperationChange} id='operator'>
+          <option value='÷'>÷</option>
+          <option value='*'>*</option>
+          <option value='+'>+</option>
+          <option value='-'>-</option>
+        </select>
+        <Game type={operation}/>
+      </div>
+    );
+
+}
