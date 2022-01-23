4d9979bf618a974a89e09bfbddb28df4ad0d8500
bornik <nik.born@gmail.com>
2022-01-23 21:58:47 +0200

    .env

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import ReactGA from 'react-ga';
 
 ReactGA.initialize('G-9N289HG8YQ');
 ReactGA.pageview(window.location.pathname + window.location.search);
-
+console.log(` ${process.env.REACT_APP_VERSION}`)
 const App = () => {
   return <Landing/>
 }
